167f9882ca7e9405aa430fcee4912732e713ad48
Saswata Patra <saswatapatra15@gmail.com>
2024-01-19 15:19:28 +0530

    [ADD] show network in navbar --reload problem not fixed

--- frontend/components/hooks/web3/useNetwork.ts
+++ frontend/components/hooks/web3/useNetwork.ts
@@ -2,24 +2,48 @@ import { CryptoHookFactory } from "@/types/hooks"
 import useSWR, { SWRConfiguration } from "swr"
 
 type UseNetworkResponse = {
-    isLoading: boolean
+    isLoading: boolean,
+    targetNetwork : string,
+    isSupported : boolean
 }
+const NETWORKS: { [k: string]: string } = {
+    1: "Ethereum Main network",
+    5: "Goerli test network",
+    11155111: "Sepolia Test Network",
+    1337: "Ganache"
 
+}
+const config :SWRConfiguration = {
+    revalidateOnFocus:false
+}
 type NetworkHookFactory = CryptoHookFactory<string, UseNetworkResponse>
 
 export type UseNetworkHook = ReturnType<NetworkHookFactory>
 
+const targetID:string = process.env.NEXT_PUBLIC_TARGET_ID as string
+const targetNetwork = NETWORKS[targetID]
+
 export const hookFactory: NetworkHookFactory = ({ provider, isLoading }) => () => {
-    const {data,isValidating,mutate,...swr} = useSWR(provider ? "web3/useNetwork" : null, async() => {
-        return "This is test Network"
-    }, {
-        revalidateOnFocus : false
-    })
+    const { data, isValidating, mutate, ...swr } = useSWR(provider ? "web3/useNetwork" : null, async () => {
+
+        const chain_id = (await provider?.getNetwork())!.chainId
+        
+        if (!chain_id){
+            throw "Cannot find network"
+        }
+        
+            
+        return NETWORKS[chain_id as unknown as number]
+    }, config)
+    console.log("DATA", data)
+    console.log("TARGET",targetNetwork)
     return {
         ...swr,
         data,
         isValidating,
-        isLoading : isLoading || isValidating,
-        mutate
+        isLoading: isLoading || isValidating,
+        mutate,
+        targetNetwork,
+        isSupported : data===targetNetwork
     }
 }
